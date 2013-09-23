478d0b8541b2fb58abe1591ac34317842b062632
Steve Grosbois <steve.grosbois@gmail.com>
2013-09-23 10:23:59 +0200

    Angular

--- js/main.js
+++ js/main.js
@@ -2,37 +2,34 @@ requirejs.config({
     baseUrl: '/js',
     paths: {
         jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
-        bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/js/bootstrap.min'
-    },
+        bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/js/bootstrap.min',
+		angular: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.min'
+	},
     shim: {
-        'bootstrap':{deps: ['jquery']}
+		'angular' : {'exports' : 'angular'},
+        'bootstrap' : {deps: ['jquery']}
     }
 });
- 
+
+/**
+ * Boot RequireJS
+ */
 requirejs([
     'jquery', 
-    'bootstrap'
-    ], function($, _bootstrap){
-	
-		$.fn.articles = function() {
-			var element = $(this);
-			$.getJSON('data/articles.json', function(data) {
-				var i = 0;
-				$.each(data.articles, function(j, item){
-					var article = $("<div/>").addClass('article').appendTo(element);
-					var h3  = $("<h3/>").html(item.titre).appendTo(article);
-					var contenu = $("<p/>").html(item.contenu).appendTo(article);
-					i++;
-				});
-			}).fail(function(jqXHR, textStatus, errorThrow) {
-                var error = $("<div/>").html(jqXHR.responseText).addClass('error').appendTo(element);
-                var textStatus = $("<div/>").html(textStatus).addClass('textStatus').appendTo(element);
-                var errorThrow = $("<div/>").html(errorThrow).addClass('errorThrow').appendTo(element);
-            });
-		}
-
-		$('#articles').articles();
-        // this is where all the site code should begin
+    'bootstrap',
+	'angular'
+    ], 
+	function(jquery, _bootstrap, angular){		
+		ArticleListCtrl.$inject = ['$scope', '$http'];
         return {};
-});
+	}
+);
 
+/**
+ * Controller ArticleListCtrl
+ */
+function ArticleListCtrl($scope, $http) {
+	$http.get('data/articles.json').success(function(data) {
+		$scope.articles = data;
+	});
+}
