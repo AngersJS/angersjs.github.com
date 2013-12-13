013ca1eba2ce1c118f58378d64f8d043b45d4a48
Steve Grosbois <steve.grosbois@gmail.com>
2013-12-13 16:05:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var AngersJS = angular.module('AngersJS', ['ngSanitize']);
  */
 AngersJS.controller('ArticleListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('data/articles.json').success(function(data) {
+        console.table(data);
         $scope.articles = data;
     });
 }]);
